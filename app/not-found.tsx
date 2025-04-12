8d7fa7a00ea91818a76f68a3835a8a4f824e108e
Silviu Stana <gamedevseals@gmail.com>
2025-04-12 11:50:08 +0300

    Panel not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,17 +1,19 @@
+'use client';
 import { PanelType } from '@/enums/PanelType';
+import { usePathname } from 'next/navigation';
 
-const NotImplementedPanel: React.FC<{
-    goToPanel: (panel: PanelType) => void;
-}> = () => (
-    <div
-        className="flex flex-col items-center
+export default function PageNotFound() {
+    const a = usePathname();
+
+    return (
+        <div
+            className="flex flex-col items-center
     max-w-md mx-auto mt-10 p-10 rounded-4xl shadow-xl bg-white"
-    >
-        <h1 className="font-bold text-red-500 text-4xl">404</h1>
-        <h2 className="text-2xl font-semibold text-center mb-4">
-            Panel not found!
-        </h2>
-    </div>
-);
-
-export default NotImplementedPanel;
+        >
+            <h1 className="font-bold text-red-500 text-4xl">404</h1>
+            <h2 className="text-2xl font-semibold text-center mb-4">
+                {`Panel "${a.substring(1)}" not found!`}
+            </h2>
+        </div>
+    );
+}
