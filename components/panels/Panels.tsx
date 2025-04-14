b7769611031759112785348f586fba826107cf20
Silviu Stana <gamedevseals@gmail.com>
2025-04-14 08:51:36 +0300

    Improved icons and design of panel flow

--- components/panels/Panels.tsx
+++ components/panels/Panels.tsx
@@ -7,18 +7,17 @@ const panelComponents: Record<
     PanelType,
     React.FC<{ goToPanel: (panel: PanelType) => void }>
 > = {
-    [PanelType.Login]: LoginPanel,
-    [PanelType.Register]: RegisterPanel,
-    [PanelType.AddFlight]: NotImplementedPanel,
-    [PanelType.ModifyFlight]: NotImplementedPanel,
-    [PanelType.BookFlight]: NotImplementedPanel,
-    [PanelType.BuyFlight]: NotImplementedPanel,
-    [PanelType.SearchFlight]: NotImplementedPanel,
-    [PanelType.FlightsList]: NotImplementedPanel,
-    [PanelType.BookSeat]: NotImplementedPanel,
-    [PanelType.DeleteFlight]: NotImplementedPanel,
-    [PanelType.Help]: NotImplementedPanel,
-    [PanelType.Exit]: NotImplementedPanel,
+    [PanelType.Login]: LoginPanel, //✅
+    [PanelType.Register]: RegisterPanel, //✅
+    [PanelType.AddFlight]: NotImplementedPanel, //
+    [PanelType.ModifyFlight]: NotImplementedPanel, //
+    [PanelType.BookFlight]: NotImplementedPanel, //
+    [PanelType.SearchFlight]: NotImplementedPanel, //
+    [PanelType.MyReservations]: NotImplementedPanel, //
+    [PanelType.SelectSeat]: NotImplementedPanel, //
+    [PanelType.DeleteFlight]: NotImplementedPanel, //
+    [PanelType.Help]: NotImplementedPanel, //
+    [PanelType.Logout]: NotImplementedPanel, //
 };
 
 export default panelComponents;
